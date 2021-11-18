a678a2accd1bd8d05a8e75cf4d4b36f2d55cbfa3
Liedel <liedel75@gmail.com>
2021-11-18 16:56:45 +0100

    add firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,33 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 FirebaseStorage storage = FirebaseStorage.instance;
+FirebaseAuth auth = FirebaseAuth.instance;
 
 //initialisation Firebase
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  getUserID();
   runApp(const MyApp());
+}
+
+getUserID() {
+  auth.authStateChanges().listen((User? user) {
+    if (user == null) {
+      try {
+        print('Utilisateur non connecté');
+        auth.signInWithEmailAndPassword(
+            email: 'test@lili.com', password: 'lollol');
+      } catch (e) {
+        print(e.toString());
+      }
+    } else {
+      print('Utilisateur connecté:' + user.email!);
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
