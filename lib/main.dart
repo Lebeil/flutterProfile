8fa4ace4011bb2024ed1c2facdca7692d7ded27c
Liedel <liedel75@gmail.com>
2021-11-18 13:48:45 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseStorage storage = FirebaseStorage.instance;
+
+//initialisation Firebase
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future uploadFile() async {
+    Reference storageRef = storage.ref('Users').child('test.png');
+    UploadTask uploadTask = storageRef.putFile(_image!);
+    // on peut mettre un loading ici
+    await uploadTask.whenComplete(() => print('File uploaded'));
+  }
+
+  Future getImage() async {
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _image == null
+                ? const Text('No image selected.')
+                : Image.file(_image!),
+            ElevatedButton(
+              child: const Text('Envoyer dans Firebase'),
+              onPressed: () => uploadFile(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: const Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
